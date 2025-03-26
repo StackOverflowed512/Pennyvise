ba4c4d9e4a726a05f28853d9f1d951382685ee8c
myselfmankar <pict.vaishnav@gmail.com>
2025-03-26 12:36:45 +0530

    updated port

--- chatbot/app.py
+++ chatbot/app.py
@@ -8,7 +8,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import numpy as np
+
 
 # Load environment variables
 load_dotenv()
@@ -240,4 +240,4 @@ def predict_stock_price_with_info():
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = False, host = '0.0.0.0', port = 5000)
